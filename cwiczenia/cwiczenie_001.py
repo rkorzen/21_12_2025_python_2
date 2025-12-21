b8512f535361a927880f0d504f10a5d2398e2f06
rkorzen <korzeniewski@gmail.com>
2025-12-21 11:39:03 +0100

    podstawy

---
+++ cwiczenia/cwiczenie_001.py
@@ -0,0 +1,21 @@
+"""
+mamy liste, np
+
+    dane = [1, 1, 2, 3, 1, 4, 5, 17, 21, 29]
+
+napisz kod  (bez definiowania funkcji)
+
+ktory z danej listy wybierze liczby pierwszze i zapisze je w nowe liscie
+
+
+...
+
+nowe_dane == [2, 3, 5, 17, 29]
+
+
+"""
+
+dane = [1, 1, 2, 3, 1, 4, 5, 17, 21, 29]
+
+
+assert nowe_dane == [2, 3, 5, 17, 29]
