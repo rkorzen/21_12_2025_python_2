f7d47be51398890255c94bfcd31b4768aa58f496
rkorzen <korzeniewski@gmail.com>
2026-01-17 09:52:05 +0100

    rekurencja

--- cwiczenia/cwiczenie_06.py
+++ cwiczenia/cwiczenie_06.py
@@ -13,11 +13,6 @@ Oraz decrypt, która odkoduje ten tekst.
 
 import string
 
-"ABCDEF...Z"
-print(ord("A") - 65 + 4, ord("a") - 97)
-print(ord("B") - 65, ord("b") - 97)
-print(chr(ord("A") - 1))
-
 def encrypt(text, shift):
     result = ""
     for char in text:
@@ -34,10 +29,20 @@ def encrypt(text, shift):
             )
     return result
 
-print(len("CEASER CIPHER DEMO"))
 
-print(len(encrypt("CEASER CIPHER DEMO", 4)))
-print(encrypt("CEASER CIPHER DEMO", 4))
-print("GIEWIV GMTLIV HIQS" == encrypt("CEASER CIPHER DEMO", 4))
+def decrypt(text, shift):
+    result = ""
+    for char in text:
+        if char not in string.ascii_letters:
+            result += char
 
+        elif char.isupper():
+            result += chr(
+                (ord(char) - shift - 65) % 26 + 65
+            )
+        else:
+            result += chr(
+                (ord(char) - shift - 97) % 26 + 97
+            )
+    return result
 
