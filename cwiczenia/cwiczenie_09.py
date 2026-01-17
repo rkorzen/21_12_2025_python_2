ebb91ee17c8892ecf6195c953ead47d2c88736c6
rkorzen <korzeniewski@gmail.com>
2026-01-17 11:56:42 +0100

    basket entry

--- cwiczenia/cwiczenie_09.py
+++ cwiczenia/cwiczenie_09.py
@@ -5,9 +5,31 @@ Stworz klase reprezentujaca produkt
 - nazwe
 - cene
 
+Stworz klase BasketEntry zawierajaca:
+- produkt
+- ilosc
 
+metoda:
+calculate - oblicza wartosc pozycji w koszyku
 
 """
 
+class Product:
+    def __init__(self, id, name, price):
+        self.id = id
+        self.name = name
+        self.price = price
+
+    def info(self):
+        return f'Id: {self.id}, Produkt: "{self.name}", cena: {self.price}'
+
+    def __repr__(self):
+        return f'<Produkt ({self.id}): {self.name} ({self.price} PLN)>'
+
+basket = []
 p = Product(1, "Ogorek", 5.34)
 assert p.info() == 'Id: 1, Produkt: "Ogorek", cena: 5.34'
+
+basket.append(p)
+print(p)
+print(basket)
