2379e71e6f4112c41698590ef7131e29c55e0fcc
rkorzen <korzeniewski@gmail.com>
2026-01-17 09:41:18 +0100

    encrypt

--- cwiczenia/cwiczenie_06.py
+++ cwiczenia/cwiczenie_06.py
@@ -5,12 +5,39 @@ Napisz funkcję encrypt, która zaszyfruje tekst zgodnie z szyfrem cesara.
 Oraz decrypt, która odkoduje ten tekst.
 
     >>> encrypt("CEASER CIPHER DEMO", 4)
-    IEWIVrGMTLIVrHIQS
-    >>> decrypt("IEWIVrGMTLIVrHIQS", 4)
-    'CEASERzCIPHERzDEMO'
-
-Wersj trudniejsza
-
-    >>> decrypt("IEWIVrGMTLIVrHIQS", 4)
+    'GIEWIV GMTLIV HIQS'
+    >>> decrypt("GIEWIV GMTLIV HIQS", 4)
     'CEASER CIPHER DEMO'
+
 """
+
+import string
+
+"ABCDEF...Z"
+print(ord("A") - 65 + 4, ord("a") - 97)
+print(ord("B") - 65, ord("b") - 97)
+print(chr(ord("A") - 1))
+
+def encrypt(text, shift):
+    result = ""
+    for char in text:
+        if char not in string.ascii_letters:
+            result += char
+
+        elif char.isupper():
+            result += chr(
+                (ord(char) + shift - 65) % 26 + 65
+            )
+        else:
+            result += chr(
+                (ord(char) + shift - 97) % 26 + 97
+            )
+    return result
+
+print(len("CEASER CIPHER DEMO"))
+
+print(len(encrypt("CEASER CIPHER DEMO", 4)))
+print(encrypt("CEASER CIPHER DEMO", 4))
+print("GIEWIV GMTLIV HIQS" == encrypt("CEASER CIPHER DEMO", 4))
+
+
