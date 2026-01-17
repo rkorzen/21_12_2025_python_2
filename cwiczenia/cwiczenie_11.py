02c468272607bc956d9a06af4d6620b81fae2454
rkorzen <korzeniewski@gmail.com>
2026-01-17 16:39:30 +0100

    zadanie 11

---
+++ cwiczenia/cwiczenie_11.py
@@ -0,0 +1,16 @@
+"""
+Zaimplementuj klase Vector - reprezentujaca wektor na plaszczyznie
+
+v = Vector(1, 2)
+v2 = Vector(3, 4)
+assert v + v2 == Vector(4, 6)
+assert v - v2 == Vector(-2, -2)
+
+v * 2 == Vector(2, 4)
+
+v > v2  # porownanie po dlugosci wektor
+reprezentacja napisowa
+porownywanie wektorow
+v == v2
+
+"""
