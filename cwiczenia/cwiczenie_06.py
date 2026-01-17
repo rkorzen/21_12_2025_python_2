a4ae23d38a542701dc4686125b89d2b1cd5a2766
rkorzen <korzeniewski@gmail.com>
2026-01-17 09:03:58 +0100

    zadanie - szyfr cezara

---
+++ cwiczenia/cwiczenie_06.py
@@ -0,0 +1,16 @@
+"""
+### Zadanie
+
+Napisz funkcję encrypt, która zaszyfruje tekst zgodnie z szyfrem cesara.
+Oraz decrypt, która odkoduje ten tekst.
+
+    >>> encrypt("CEASER CIPHER DEMO", 4)
+    IEWIVrGMTLIVrHIQS
+    >>> decrypt("IEWIVrGMTLIVrHIQS", 4)
+    'CEASERzCIPHERzDEMO'
+
+Wersj trudniejsza
+
+    >>> decrypt("IEWIVrGMTLIVrHIQS", 4)
+    'CEASER CIPHER DEMO'
+"""
