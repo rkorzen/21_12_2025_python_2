6e08cd29054ec393da60058bc0232ea4d9a7011c
rkorzen <korzeniewski@gmail.com>
2026-01-17 11:42:28 +0100

    klasy - wprowadzenie

--- cwiczenia/cwiczenie_08.py
+++ cwiczenia/cwiczenie_08.py
@@ -3,22 +3,56 @@
 Napisz dekorator, ktory bedzie logowac uzycie funkcji, jej nazwe, argumenty, wynik i czas wykonania
 
 """
+import logging
+import sys
+import time
+from functools import wraps
+
+sys.set_int_max_str_digits(100000)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(message)s",
+    filename="log.txt"
+)
+logger = logging.getLogger(__name__)
+
 
 def logowanie(func):
-    return func
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        t1 = time.time()
+        try:
+            result = func(*args, **kwargs)
+        except Exception as e:
+            logger.error(
+                f"Error in function `{func.__name__}` with args: pozycyjne: {args}, nazwane: {kwargs}. Error: {e}",
+                exc_info=True, stack_info=True)
+            raise
+        log = f"Wywołano funkcję: `{func.__name__}` z parametrami: pozycyjne: {args}, nazwane: {kwargs} wynik: {str(result)[:10]}, czas: {time.time() - t1:.2f}s"
+        logger.info(log)
+        return result
+
+    return wrapper
+
 
 @logowanie
-def jakas_funkcja():
-    ...
+def add(a, b):
+    return a + b
 
-jakas_funkcja()
 
-""" Powinno to printowac
-Wywolano funkcje `jakas_funkcja` z parametrami: None, wynik: None, czas: 0.000000s
-"""
+@logowanie
+def bar(n=10):
+    return sum(x ** n for x in range(n))
 
-import time
-t1 = time.time()
-time.sleep(1)
-t2 = time.time()
-print(f"Czas: {t2-t1}s")
+
+@logowanie
+def bad_function():
+    1 / 0
+
+
+add(10, 20)
+
+bar(1000)
+
+bad_function()
---
+++ cwiczenia/cwiczenie_09.py
@@ -0,0 +1,12 @@
+"""
+Stworz klase reprezentujaca produkt
+
+- id
+- nazwe
+- cene
+
+
+
+"""
+
+p = Product(1, "Ogorek", 5.34)
