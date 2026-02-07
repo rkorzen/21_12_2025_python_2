9ea1fe7b16b9d8c3f0b788057db056db5cbce05d
rkorzen <korzeniewski@gmail.com>
2026-02-07 17:00:26 +0100

    m2m

--- django_examples/hello_django/news/models.py
+++ django_examples/hello_django/news/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 
 class TimeStampedModel(models.Model):
@@ -16,16 +17,19 @@ class News(TimeStampedModel):
     is_published = models.BooleanField(default=False)
     pub_date = models.DateTimeField(null=True)
     author = models.ForeignKey('Author', on_delete=models.CASCADE, null=True, related_name='news')
+    tags = models.ManyToManyField('Tag', related_name='news')
 
     def __str__(self):
         author_name = self.author.short_name if self.author else "anonimowy"
         return f"{self.title} ({author_name} - {self.pub_date.strftime('%Y-%m-%d %H:%M:%S')})"
+
 
 class Author(TimeStampedModel):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     birth_date = models.DateField(null=True)
     death_date = models.DateField(null=True)
+    tags = models.ManyToManyField('Tag', related_name='authors')
 
     @property
     def short_name(self):
@@ -35,3 +39,25 @@ class Author(TimeStampedModel):
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
+
+import unicodedata
+
+
+def slugify(text: str) -> str:
+    text = text.lower().replace("ł", "l").replace(" ", "-")
+    return (
+        unicodedata
+        .normalize('NFKD', text)
+        .encode('ascii', 'ignore')
+        .decode('ascii')
+    )
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        if self.slug is None:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
