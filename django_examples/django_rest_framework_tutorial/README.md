8fc72ea5c4d4a573fa99019ebc622ebec1b898d1
rkorzen <korzeniewski@gmail.com>
2026-02-22 10:19:28 +0100

    sierializer

--- django_examples/django_rest_framework_tutorial/README.md
+++ django_examples/django_rest_framework_tutorial/README.md
@@ -9,3 +9,8 @@ author: str
 status: choices
 visible: bool
 created: datetime (auto_now_add=True)
+
+## stworz serializer dla Book - zapisywanie i odczytywanie danych
+po stworzeniu w shell utworz:
+
+utworz ksiazke, zserializuj ksiazke, utworz ksiazke przy pomocy serializera, zserializuj wszystkie ksiazki...
--- django_examples/django_rest_framework_tutorial/books/models.py
+++ django_examples/django_rest_framework_tutorial/books/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+
+    class Status(models.TextChoices):
+        AVAILABLE = 'av', 'Available'
+        RENT = 're', 'Rent'
+        ARCHIVED = 'ar', 'Archived'
+
+
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    status = models.CharField(
+        max_length=2,
+        choices=Status.choices,
+        default=Status.AVAILABLE
+    )
+    visible = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+
