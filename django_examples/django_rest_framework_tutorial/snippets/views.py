dc75df5d87c189fe496f28eb8b030b97001c7a49
rkorzen <korzeniewski@gmail.com>
2026-02-22 14:27:53 +0100

    widoki generyczne

--- django_examples/django_rest_framework_tutorial/snippets/views.py
+++ django_examples/django_rest_framework_tutorial/snippets/views.py
@@ -1,53 +1,14 @@
 from rest_framework import generics
-from rest_framework import mixins
 
 from .models import Snippet
 from .serializers import SnippetSerializer
 
 
-# Create your views here.
-
-# GET /snippets
-# POST /snippets
-class SnippetList(
-    mixins.ListModelMixin,
-    mixins.CreateModelMixin,
-    generics.GenericAPIView
-):
+class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self, request):
-        return self.list(request)
 
-    def post(self, request):
-        return self.create(request)
-
-
-"""
-Content-Type: application/json  # text/html
-
-"""
-
-
-# GET /snippets/<int:pk>
-# PUT /snippets/<int:pk>
-# DELETE /snippets/<int:pk>
-# noinspection PyStubPackagesAdvertiser
-class SnippetDetail(
-    mixins.RetrieveModelMixin,
-    mixins.UpdateModelMixin,
-    mixins.DestroyModelMixin,
-    generics.GenericAPIView
-):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
-    def get(self, request, pk):
-        return self.retrieve(request, pk)
-
-    def put(self, request, pk):
-        return self.update(request, pk)
-
-    def delete(self, request, pk):
-        return self.destroy(request, pk)
