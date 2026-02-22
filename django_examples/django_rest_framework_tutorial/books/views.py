b39d9077596acdc473fe056a4240a3f5b59a6e16
rkorzen <korzeniewski@gmail.com>
2026-02-22 14:34:56 +0100

    generyczne widoki w Books

--- django_examples/django_rest_framework_tutorial/books/views.py
+++ django_examples/django_rest_framework_tutorial/books/views.py
@@ -7,34 +7,15 @@ from .serializers import BookSerializer
 
 # Create your views here.
 
-class BookList(
-    mixins.ListModelMixin,
-    mixins.CreateModelMixin,
-    generics.GenericAPIView):
+class BookList(generics.ListCreateAPIView):
 
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-    def get(self, request):
-        return self.list(request)
 
-    def post(self, request):
-        return self.create(request)
 
-class BookDetail(
-    mixins.RetrieveModelMixin,
-    mixins.UpdateModelMixin,
-    mixins.DestroyModelMixin,
-    generics.GenericAPIView):
+class BookDetail(generics.RetrieveUpdateDestroyAPIView):
 
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-    def get(self, request, pk):
-        return self.retrieve(request, pk)
-
-    def put(self, request, pk):
-        return self.update(request, pk)
-
-    def delete(self, request, pk):
-        return self.destroy(request, pk)
--- django_examples/django_rest_framework_tutorial/snippets/views.py
+++ django_examples/django_rest_framework_tutorial/snippets/views.py
@@ -5,6 +5,18 @@ from .serializers import SnippetSerializer
 
 
 class SnippetList(generics.ListCreateAPIView):
+    """
+    List all snippets, or create a new snippet.
+
+    This view set automatically provides `list` and `create` actions.
+
+    ## List snippets
+    * a
+    * b
+
+
+    """
+
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
