19246db86a8a41cefdb1376af47a67a7b58177a4
rkorzen <korzeniewski@gmail.com>
2026-02-22 12:33:26 +0100

    api_view

--- django_examples/django_rest_framework_tutorial/snippets/views.py
+++ django_examples/django_rest_framework_tutorial/snippets/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from rest_framework import status
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
 from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
 from .models import Snippet
 from .serializers import SnippetSerializer
 from django.http import JsonResponse, HttpResponseNotAllowed, HttpResponse
@@ -10,20 +13,20 @@ from django.http import JsonResponse, HttpResponseNotAllowed, HttpResponse
 
 # GET /snippets
 # POST /snippets
-@csrf_exempt
+@api_view(["GET", "POST"])
 def snippet_list(request):
     if request.method == "GET":
         snippets = Snippet.objects.all()
         serializer = SnippetSerializer(snippets, many=True)
-        return JsonResponse(serializer.data, safe=False)
-    elif request.method == "POST":
-        data = JSONParser().parse(request)
-        serializer = SnippetSerializer(data=data)
+        return Response(serializer.data)
+
+    if request.method == "POST":
+        serializer = SnippetSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
-    return HttpResponseNotAllowed(["GET", "POST"])
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 
 """
@@ -35,30 +38,25 @@ Content-Type: application/json  # text/html
 # GET /snippets/<int:pk>
 # PUT /snippets/<int:pk>
 # DELETE /snippets/<int:pk>
-@csrf_exempt
+@api_view(["GET", "PUT", "DELETE"])
 def snippet_detail(request, pk):
-    ALLOWED_METHODS = ["GET", "PUT", "DELETE"]
-
-    if request.method not in ALLOWED_METHODS:
-        return HttpResponseNotAllowed(ALLOWED_METHODS)
 
     try:
         snippet = Snippet.objects.get(pk=pk)
     except Snippet.DoesNotExist:
-        return HttpResponse(status=404)
+        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == "GET":
         serializer = SnippetSerializer(snippet)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
     if request.method == "PUT":
-        data = JSONParser().parse(request)
-        serializer = SnippetSerializer(snippet, data=data)
+        serializer = SnippetSerializer(snippet, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     if request.method == "DELETE":
         snippet.delete()
-        return HttpResponse(status=204)
+        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
