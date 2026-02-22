6eeb7c169a05d6528aef049c149f5a4d9ef76f56
rkorzen <korzeniewski@gmail.com>
2026-02-22 12:22:05 +0100

    Book detail view

--- django_examples/django_rest_framework_tutorial/books/urls.py
+++ django_examples/django_rest_framework_tutorial/books/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.book_list, name="book-list")
+    path("", views.book_list, name="book-list"),
+    path("<int:pk>", views.book_detail, name="book-detail"),
 ]
--- django_examples/django_rest_framework_tutorial/books/views.py
+++ django_examples/django_rest_framework_tutorial/books/views.py
@@ -1,4 +1,4 @@
-from django.http import JsonResponse, HttpResponseNotAllowed
+from django.http import JsonResponse, HttpResponseNotAllowed, HttpResponse
 from django.template.context_processors import csrf
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
@@ -27,3 +27,33 @@ def book_list(request):
             serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
+
+
+
+@csrf_exempt
+def book_detail(request, pk):
+    ALLOWED_METHODS = ["GET", "PUT", "DELETE"]
+
+    if request.method not in ALLOWED_METHODS:
+        return HttpResponseNotAllowed(ALLOWED_METHODS)
+
+    try:
+        snippet = Book.objects.get(pk=pk)
+    except Book.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == "GET":
+        serializer = BookSerializer(snippet)
+        return JsonResponse(serializer.data)
+
+    if request.method == "PUT":
+        data = JSONParser().parse(request)
+        serializer = BookSerializer(snippet, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    if request.method == "DELETE":
+        snippet.delete()
+        return HttpResponse(status=204)
